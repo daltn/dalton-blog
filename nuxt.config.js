fc20da7b8f2e59f09468cbe251d25b95e5a1eb45
Dalton Saffe <daltonsaffe@gmail.com>
2021-06-01 23:18:18 -0400

    add smiley

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'dalton saffe',
+    title: 'dalton saffe :)',
     htmlAttrs: {
       lang: 'en',
     },
