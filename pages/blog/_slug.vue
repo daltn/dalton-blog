be917b6db25ecdbdccb3eac737328a407d3c8ebe
Dalton Saffe <daltonsaffe@gmail.com>
2021-06-11 18:29:22 -0400

    add date border

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="container">
     <h1>{{ article.title }}</h1>
-    <p>{{ formatDate(article.updatedAt) }}</p>
+    <p class="date">{{ formatDate(article.updatedAt) }}</p>
     <nuxt-content :document="article" />
   </article>
 </template>
@@ -22,4 +22,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.date {
+  padding-bottom: 5px;
+  border-bottom: 1px solid rgb(125, 238, 204);
+}
+</style>
